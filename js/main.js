07cc9fc46c49ca7fde6afa744b794495c4485fee
IT Student <atc01@atc.local>
2019-10-05 08:57:20 +0200

    Testimi i pare git

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var totali = "Hello World";
+var mosha = 23;
+var mosha2 = 25;
+totali = mosha + mosha2;
+
+
+function myFunction(){
+	console.log(totali);
+}
+
+function kuJemi(){
+	var text = document.getElementById("klikuar");
+	console.log(text);
+}
+
